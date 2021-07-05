fc35d1cbdcbafd6942de875449dc37a56717bafe
arcodev <chris_arco0794@hotmail.com>
2021-07-05 13:34:19 -0500

    Agregado la libreria ant design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.scss';
+import { DatePicker } from 'antd';
 function App() {
   return (
     <div className="app">
       <h1>Curso de mern</h1>
       <h2>Hola mundo</h2>
+      <DatePicker />
     </div>
   );
 }
-
 export default App;
