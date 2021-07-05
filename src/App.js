8a263312f3a1ad4296b849aa0dc17cb479ac3a2e
arcodev <chris_arco0794@hotmail.com>
2021-07-05 18:18:26 -0500

    Creacion de la paginas para realizar las rutas

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
 import './App.scss';
-/*Con "as" le agragamos un alias al componte */
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+/*Con "as" le agragamos un alias al componte import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'*/
+import Admin from './pages/Admin';
+import SigIn from './pages/Admin/SignIn';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+
+
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <h1>Sistema de rutas basico</h1>
-        <Link to="/">Home</Link> <br />
-        <Link to="/contact">Contact</Link><br /> 
-        <Link to="/users">Users</Link><br />
-        <br /><br /><br />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/users" component={User} />
-          <Route component={Error404} />
-        </Switch>
+      <div>
+        <h1>Estamos en App.js</h1>
+        <Admin />
+        <SigIn />
+        <Home />
+        <Contact />
       </div>
-    </Router>
   );
 }
 
-function Home() {
-  return <h2>Estamos en el comoponente home</h2>;
-}
-function Contact() {
-  return <h3>Estamos en el comoponente contact</h3>;
-}
-function User() {
-  return <h4>Estamos en el comoponente users</h4>;
-}
-
-function Error404() {
-  return <h3>Error404...</h3>;
-}
 export default App;
